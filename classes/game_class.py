a29f2252cdcf1e6999651e67f8b24e2ef4b9600b
Kirill <74716631+BeastMasta64@users.noreply.github.com>
2023-03-18 18:22:04 +0300

    Update game_class.py

--- classes/game_class.py
+++ classes/game_class.py
@@ -1,49 +1,74 @@
+import random
 from typing import List
 
 from classes.deck_class import Deck
+from classes.fight_class import Fight
 from classes.player_class import Player
+from classes.tavern_class import Tavern
 
 
 class Game:
+    max_turn_number = 100
 
     def __init__(self, players: List[Player], deck: Deck):
         self.players = players
         self.turn_number = 1
         self.deck = deck
 
-    def play_game(self):
-        game = Game()
-        print('Would u like to name players? 1 - Yes, 0 - No')
-        agreement = int(input())
-        if agreement == 1:
-            print('Enter players names like this "Player1 Player2"')
-            players_names = [player_name for player_name in input().split()]
-            players_taverns = []
-            for i in range(self.players_number):
-                players_taverns.append(Tavern(game, player_name=players_names[i]))
-        else:
-            players_taverns = []
-            for i in range(self.players_number):
-                players_taverns.append(Tavern(game, player_name='Player' + str(i + 1)))
-        while len(players_taverns) > 1:
-            for player in players_taverns:
-                player.player_turn()
-            fighting_spisok = list(players_taverns)
-            while len(fighting_spisok) > 1:
-                fighter1, fighter2 = random.sample(fighting_spisok, 2)
-                fighting_spisok.remove(fighter1)
-                fighting_spisok.remove(fighter2)
-                player_won, damage_dealt = Fight(fighter1, fighter2).simulate()
-                if player_won == -1:
-                    pass
-                elif player_won == 0:
-                    fighter2.player_hp -= damage_dealt
-                elif player_won == 1:
-                    fighter1.player_hp -= damage_dealt
-                else:
-                    print('error')
-            for player in players_taverns:
-                if player.player_hp <= 0:
-                    players_taverns.remove(player)
+    @property
+    def alive_players(self) -> List[Player]:
+        return [player for player in self.players if player.health > 0]
 
-        print(f'Player {players_taverns[0].player_name} won')
+    @property
+    def dead_players(self) -> List[Player]:
+        return [player for player in self.players if player.health <= 0]
+
+    def return_fighting_list(self) -> List[Player]:
+        if len(self.alive_players) % 2 == 0:
+            return self.alive_players
+        else:
+            return self.alive_players + [random.choice(self.dead_players)]
+
+    def make_tavern_turn(self, player: Player):
+        tavern = Tavern(player=player, deck=self.deck, turn_number=self.turn_number)
+        print(f'Player: {player.name} turn')
+        while True:
+            print(f'Print action number: \n'
+                  f'1 - buy a minion, \n'
+                  f'2 - play a card, \n'
+                  f'3 - sell a minion, \n'
+                  f'4 - reroll, \n'
+                  f'5 - show stats, \n'
+                  f'6 - end the turn')
+            action_number = int(input())
+            action_dictionary = {1: 'Buy minion', 2: 'Play card', 3: 'Sell minion', 4: 'Reroll', 5: 'Stats'}
+            if action_number in action_dictionary:
+                tavern.player_turn(action=action_dictionary[action_number])
+            elif action_number == 6:
+                tavern.clear_board()
+                break
+            else:
+                print('Wrong input')
+
+    def do_tavern_cycle(self):
+        for player in self.alive_players:
+            self.make_tavern_turn(player=player)
+
+    def do_fighting_cycle(self):
+        fighting_spisok = self.return_fighting_list()
+        print('Fighting cycle begins')
+        for i in range(len(fighting_spisok) // 2):
+            fighter1, fighter2 = random.sample(fighting_spisok, 2)
+            fighting_spisok.remove(fighter1)
+            fighting_spisok.remove(fighter2)
+            fight = Fight(first_player=fighter1, second_player=fighter2)
+            fight.simulate()
+
+    def play_game(self):
+        for i in range(self.max_turn_number):
+            if len(self.alive_players) < 2:
+                break
+            self.do_tavern_cycle()
+            self.do_fighting_cycle()
+
+            self.turn_number += 1

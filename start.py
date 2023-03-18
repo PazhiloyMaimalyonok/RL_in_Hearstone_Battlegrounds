f19b9b3dfa571df8eba419ed15ebaa908d7131ea
Kirill <74716631+BeastMasta64@users.noreply.github.com>
2023-03-18 18:25:45 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,17 @@
+from classes.deck_class import Deck
+from classes.game_class import Game
+from classes.player_class import Player
+
+
+def start_game():
+    players_number = 2
+    players = []
+    for i in range(1, players_number + 1):
+        players.append(Player(name=f'Player {i}'))
+    deck = Deck(minions_information_file_path='minions_information.csv')
+    game = Game(players=players, deck=deck)
+    game.play_game()
+
+
+if __name__ == '__main__':
+    start_game()
